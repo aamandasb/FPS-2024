d407e7d942f46bca0a1a10435c83c112bd9458b5
aamandasb <161627975+aamandasb@users.noreply.github.com>
2024-06-13 11:34:25 -0300

    Update Player Controller

--- FPS 2024/Assets/Scripts/PlayerController.cs
+++ FPS 2024/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    CharacterController characterController;
+    PlayerGravity playerGravity ;
+    Transform camTransform;
+    Weapon weapon;
+    GameObject grenade;
+    Transform throwPoint;
+
+    Vector3 direction;
+    Vector2 camDirection;
+
+    const float speed = 0;
+    float verticalRotation, mouseSensitivity;
+    bool shooting, crouching;
+    
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        characterController = GetComponent<CharacterController>();
+        camTransform = gameObject.transform.GetChild(0);
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
